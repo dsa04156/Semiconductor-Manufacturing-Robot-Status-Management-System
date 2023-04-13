4c4913cff52fe349504afdfc52b19320565f367d
이재훈 <dlwogns1193@naver.com>
2023-04-14 08:48:12 +0900

    Docs : 초기세팅

---
+++ frontend/src/Presentational/Pages/MainPage.jsx
@@ -0,0 +1,16 @@
+// 예시
+import styled from "styled-components";
+
+const Mainpage = () => {
+    
+    return (
+        <Test>테스트</Test>
+        // component들 불러오기
+    );
+};
+
+export default Mainpage;
+
+const Test = styled.div`
+  color: red;  
+`;
