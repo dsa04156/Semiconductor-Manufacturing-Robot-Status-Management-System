15cf87e2b6d59f6d4dd26a662b8b24060bed5862
이재훈 <dlwogns1193@naver.com>
2023-04-19 17:58:02 +0900

    feat: graph

--- frontend/src/Presentational/Component/MainPage/HealthStatus.jsx
+++ frontend/src/Presentational/Component/MainPage/HealthStatus.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import ReactApexChart from "react-apexcharts";
 
 const HealthStatus = () => {
-
+  const donutData = {
+    series: [50, 40, 30, 10, 0],
+    options: {
+      chart: {
+        type: "donut",
+      },
+      legend: {
+        position: "bottom",
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+        },
+      ],
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              total: {
+                showAlways: true,
+                show: true,
+                label: "ALARM",
+                fontSize: "12px",
+                color: "red",
+              },
+              value: {
+                fontSize: "22px",
+                show: true,
+                color: "blue",
+              },
+            },
+          },
+        },
+      },
+      labels: ["침입", "배회", "쓰러짐", "화재", "안전모"],
+      title: {
+        text: "이벤트별 통계",
+        align: "center",
+      },
+    },
+  };
   return <div>
-
+            <ReactApexChart 
+            options={donutData.options}
+            series={donutData.series}
+            type="donut" 
+            width="500"
+        />
   </div>;
 };
 
