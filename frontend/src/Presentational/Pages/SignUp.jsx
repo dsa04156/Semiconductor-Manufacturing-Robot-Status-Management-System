ef16787abc95c7628d7af0a2966c392f625361ff
이재훈 <dlwogns1193@naver.com>
2023-04-28 14:19:49 +0900

    feat: 로그인,로그아웃

--- frontend/src/Presentational/Pages/SignUp.jsx
+++ frontend/src/Presentational/Pages/SignUp.jsx
@@ -1,21 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
-import jwtDecode from "jwt-decode";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { useDispatch } from "react-redux";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import api from '../../redux/api'
-
-
+import api from "../../redux/api";
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-  
-  const dispatch = useDispatch();
 
   const [isMounted, setIsMounted] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setIsMounted(true);
@@ -24,27 +21,25 @@ const SignUp = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { isSubmitting, isDirty, errors },
   } = useForm();
 
+  const password = useRef();
+  password.current = watch("password");
 
-
-  const onSubmit = handleSubmit(({ email, password }) => {
-    api.post("/account/login",JSON.stringify({email,password}))
+  const onSubmit = handleSubmit(({ email, name, password, phone }) => {
+    api
+      .post("/account/join", JSON.stringify({ email, name, password, phone}))
       .then((response) => {
-        if (response.status === 401) {
-          alert(response.message)
-        }
-        else {
-          console.log(response)
-        }
-      }).catch((error) => {
-      console.log(error)
-    })
-
+        alert(response.data)
+        navigate("/")
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error.response.data);
+      });
   });
-
-
 
   if (!isMounted) {
     return null;
@@ -58,7 +53,7 @@ const SignUp = () => {
       </Col>
       <Col>
         <div className="signup_back">
-          <Form>
+          <Form onSubmit={onSubmit}>
             <div className="bold_header">회원가입</div>
             <Form.Group className="mb-3">
               <Form.Label htmlFor="email">아이디</Form.Label>
@@ -66,51 +61,90 @@ const SignUp = () => {
                 autoFocus={true}
                 htmlSize={50}
                 id="email"
-                type="text"
+                type="email"
                 placeholder="balamia@wonik.co.kr"
                 aria-invalid={!isDirty ? undefined : errors.email ? "true" : "false"}
                 {...register("email", {
                   required: "이메일은 필수 입력입니다.",
                   pattern: {
-                    value: /\S+@\S+\.\S+/,
+                    value: /^\S+@\S+\.\S+/,
                     message: "이메일 형식에 맞지 않습니다.",
                   },
                 })}
               />
+              {errors.email && <Alert>{errors.email.message}</Alert>}
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>비밀번호</Form.Label>
               <Form.Control
-              id="password"
-              type="password"
-              placeholder="********"
-              aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
-              {...register("password", {
-                required: "비밀번호는 필수 입력입니다.",
-                minLength: {
-                  value: 8,
-                  message: "8자리 이상 비밀번호를 사용하세요.",
-                },
-              })}
-            />
-            {errors.password && <small role="alert">{errors.password.message}</small>}
-          
+                id="password"
+                type="password"
+                placeholder="********"
+                aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
+                {...register("password", {
+                  required: true,
+                  minLength: {
+                    value: 8,
+                    message: "8자리 이상 비밀번호를 사용하세요.",
+                  },
+                })}
+              />
+              {errors.password && <small role="alert">{errors.password.message}</small>}
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>비밀번호 확인</Form.Label>
               <Form.Control
+                id="password_conform"
                 type="password"
                 className="form-control"
                 placeholder="********"
+                aria-invalid={!isDirty ? undefined : errors.password_conform ? "true" : "false"}
+                {...register("password_conform", {
+                  required: true,
+                  validate: (value) => value === password.current,
+                })}
               />
+              {errors.password_conform && errors.password_conform.type ==="validate" && <Alert>The passwords do not match</Alert>}
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>이름</Form.Label>
-              <Form.Control type="password" className="form-control" placeholder="Enter name" />
+              <Form.Control
+                id="name"
+                type="text"
+                className="form-control"
+                placeholder="Enter name"
+                aria-invalid={!isDirty ? undefined : errors.name ? "true" : "false"}
+                {...register("name", {
+                  required: "이름은 필수 입력입니다.",
+                  pattern: {
+                    value: /^[가-힣a-zA-Z0-9]+$/,
+                    message: "한글, 영어만 사용 가능합니다.",
+                  },
+                })}
+              />
+              {errors.name && <Alert>{errors.name.message}</Alert>}
+
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>전화번호</Form.Label>
-              <Form.Control type="password" className="form-control" placeholder="Enter phone number" />
+              <Form.Control
+                id="phone"
+                type="text"
+                placeholder="010-0000-0000"
+                aria-invalid={!isDirty ? undefined : errors.phone ? "true" : "false"}
+                {...register("phone", {
+                  required: "전화번호는 필수 입력입니다.",
+                  pattern: {
+                    value: /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/,
+                    message: "알맞은 전화번호 형태를 입력해주세요 EX) 010-9994-2223",
+                  },
+                  minLength: {
+                    value: 13,
+                    message: "알맞은 전화번호 형태를 입력해주세요. EX) 010-9994-2223",
+                  },
+                })}
+              />
+               {errors.phone && <Alert role="alert">{errors.phone.message}</Alert>}
             </Form.Group>
             <div className=" d-grid gap-2 mt-4">
               <Button size="lg" type="submit" disabled={isSubmitting} className="button">
@@ -128,3 +162,7 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
+const Alert = styled.div`
+  color : red
+`
