502fb83cd51980b6e5d23aaa052f4ac18d273a46
LEESEOKKI <116289302+LEESEOKKI@users.noreply.github.com>
2023-05-17 12:15:42 +0900

    frontend

---
+++ frontend/src/Presentational/Component/MainPage/ComponentList.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useMemo } from "react";
+import styled from "styled-components";
+import CreateTable from "../../common/CreateTable";
+import HealthStatus from "./HealthStatus";
+
+const ComponentList = ({ child, setSelectComponentData }) => {
+  const [test, setTest] = useState(0); //component list idx 값 저장
+  setSelectComponentData(child[test]);
+
+  return (
+    <div>
+      <Small>
+        <Font>COMPONENT LIST</Font>
+        <Line/>
+          <CreateTable setTest={setTest} data={child} test={test} child={child} />
+      </Small>
+      <HealthStatus componentData={child} />
+    </div>
+  );
+};
+
+export default ComponentList;
+
+const Small = styled.div`
+  position: absolute;
+  top: 260px;
+  left: 120px;
+  background: #ffffff;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  width: 460px;
+  height: 470px;
+  border-radius: 20px;
+  overflow-y: hidden;
+`;
+
+const Font = styled.div`
+  margin: 12px 0px 12px 25px;
+  font-family: "Inter";
+  font-style: normal;
+  font-size: 16px;
+  color: #707070;
+`;
+
+const Line = styled.div`
+  position: absolute;
+  width: 430px;
+  height: 0px;
+  left: 15px;
+  top: 45px;
+  border: 1px solid #707070;
+`;
