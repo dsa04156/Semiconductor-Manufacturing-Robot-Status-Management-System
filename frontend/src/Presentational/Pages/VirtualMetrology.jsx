9dc626bd99f3697cafa4ca5ee284c331b654b686
JungJinUk <dsa04156@naver.com>
2023-05-19 11:59:40 +0900

    12

--- frontend/src/Presentational/Pages/VirtualMetrology.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useRef, useState } from "react";
-import styled from "styled-components";
-import * as d3 from "d3";
-
-const VirtualMetrology = () => {
-  return <div></div>;
-};
-
-export default VirtualMetrology;
